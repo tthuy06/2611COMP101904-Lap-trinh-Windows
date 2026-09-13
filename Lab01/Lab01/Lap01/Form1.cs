03283897a66f8cc7ce6c082b104af2f38a677a57
Nguyễn Thanh Thủy <thuyt.cnthongtin@gmail.com>
2026-09-13 23:59:00 +0700

    Ket noi lai va cap nhat ma nguon

---
+++ Lab01/Lab01/Lap01/Form1.cs
@@ -0,0 +1,20 @@
+namespace Lap01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lstResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstResult_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
